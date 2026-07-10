e0a5998a1bcb23b137bbaca43ebbd219046aa368
gg-prince <g.prince@alustudent.com>
2026-07-10 11:35:53 -0700

    Updated task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -19,6 +19,5 @@ def roman_to_int(roman_string):
             total -= current_value
         else:
             total += current_value
-            
         prev_value = current_value
     return total
