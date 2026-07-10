cb270c26e53251a750eb96ced00c0e64f2f66b97
gg-prince <g.prince@alustudent.com>
2026-07-10 11:34:34 -0700

    Updated task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,6 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-
     roman_map = {
         'I': 1,
         'V': 5,
@@ -12,19 +11,14 @@ def roman_to_int(roman_string):
         'D': 500,
         'M': 1000
     }
-
     total = 0
     prev_value = 0
-
     for char in reversed(roman_string):
         current_value = roman_map.get(char, 0)
-        
         if current_value < prev_value:
             total -= current_value
         else:
             total += current_value
             
         prev_value = current_value
-
     return total
-
