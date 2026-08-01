eedac7872a8bd947e110043422f663a86a1cd208
gg-prince <g.prince@alustudent.com>
2026-08-01 21:10:35 +0000

    Added network 1 tasks

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Fetches a URL and displays information about its response body."""
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode("utf-8"))
